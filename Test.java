4d9638ef6380a1b36ce25ecb2d641dd20bf0013d
Unknown <wjbjimmy@gmail.com>
2018-01-13 16:14:57 -0500

    delete nancy

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello");
-    System.out.println("Alice");
-    System.out.println("Nancy");
+    	System.out.println("Alice");
 		System.out.println("jingbo wang");
 	}
 }
