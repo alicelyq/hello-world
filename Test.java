5ded26f53441485bc51954d4653d422d17ce83a6
Yunqi(Alice) <alicelyq@outlook.com>
2018-01-13 16:18:15 -0500

    Genius

--- Test.java
+++ Test.java
@@ -1,8 +1,9 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello");
-    System.out.println("Alice");
-    System.out.println("Nancy");
+        System.out.println("Alice");
+        System.out.println("Nancy");
 		System.out.println("jingbo wang");
+        System.out.println("I'm a genius");
 	}
 }
